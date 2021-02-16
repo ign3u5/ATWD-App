6e2b3cfca46a25e7a5ce5dab8c9e95cf30c1acfd
Chris Farmer <WS311471@weston.ac.uk>
2021-02-16 10:25:50 +0000

    Moving init options to a class

--- src/app/pageElements/editor/editor.component.ts
+++ src/app/pageElements/editor/editor.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { observable, Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CMSStorageService } from 'src/app/shared/cms/cmsStorageService';
+import { InitOptions } from 'src/app/shared/models/editor-init-options';
 
 @Component({
 	selector: 'editorc',
@@ -14,15 +15,20 @@ export class EditorComponent implements OnChanges{
     public isEditor: boolean;
     public apiKey: string;
     public content: string;
-    public content$: Observable<string>;
 
     @Input() pageName: string;
     @Input() editorId: number;
+    @Input() initOptions: InitOptions;
 
     constructor(private cmsService: CMSStorageService) {
         this.apiKey = "t3yjki3k9abk4gc0ghgkc0yzuxr8cgm80mefx3uh1ps0u7fd";
         this.isEditor = true;
-
+        this.initOptions = new InitOptions(500, 
+            false, 
+            "quicktable image media codesample", 
+            "underline | formatselect | blockquote quicklink", 
+            "undo redo | insertable | cell row column deletable | help");
+        
     }
     ngOnChanges()
     {
---
+++ src/app/shared/models/editor-init-options.ts
@@ -0,0 +1,7 @@
+export class InitOptions{
+    constructor(public height: number,
+        public menubar: boolean, 
+        public quickbars_insert_toolbar: string, 
+        public quickbars_selection_toolbar: string, 
+        public contextmenu: string) {}
+}
