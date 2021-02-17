20ad03f8dda0feaab64ecb6e5c09144bdfa1befd
Chris Farmer <WS311471@weston.ac.uk>
2021-02-17 10:53:37 +0000

    Working on gallery, only saving last gallery caption

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,15 +1,22 @@
 import { Component } from "@angular/core";
 import { NavigationStart } from '@angular/router';
 import { CMSStorageService } from "src/app/shared/cms/cmsStorageService";
+import { SetupOptions } from "src/app/shared/models/editor-init-options";
+import { EditorFactoryService } from "src/app/shared/services/editor-factory/editor-factory.service";
 
 @Component({
     selector: 'gallery',
-    templateUrl: './gallery.component.html'
+    templateUrl: './gallery.component.html',
+    styleUrls: [
+        'gallery.component.scss'
+    ]
 })
 export class GalleryComponent {
     public pageName: string;
+    public galleryCaptionOptions: SetupOptions;
 
-    constructor(private cmsService: CMSStorageService){
+    constructor(private cmsService: CMSStorageService, private editorFactory: EditorFactoryService){
         this.cmsService.loadPage("gallery").subscribe(pageName => this.pageName = pageName);
+        this.galleryCaptionOptions = editorFactory.getSubTitleOptions();
     }
 }
