c36097d1dce370e2e02ae722a6784146cdf712e9
Chris Farmer <WS311471@weston.ac.uk>
2021-03-17 15:48:23 +0000

    Adding contact form

--- src/app/pages/contactUs/contactUs.component.ts
+++ src/app/pages/contactUs/contactUs.component.ts
@@ -1,14 +1,29 @@
 import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 import { CMSStorageService } from "src/app/shared/cms/cmsStorageService";
 
 @Component({
     selector: 'contactUs',
-    templateUrl: './contactUs.component.html'
+    templateUrl: './contactUs.component.html',
+    styleUrls: ['./contactUs.component.scss']
 })
 export class ContactUsComponent {
     public pageName: string;
+    public contactForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        company: [''],
+        email: ['', [Validators.required, Validators.email]],
+        message: ['', Validators.required],
+    });
 
-    constructor(private cmsService: CMSStorageService){
+    constructor(private cmsService: CMSStorageService, private formBuilder: FormBuilder){
         this.cmsService.loadPage("contactUs").subscribe(pageName => this.pageName = pageName);
+    }
+
+
+    public onSubmit()
+    {
+        console.log(`Hello ${this.contactForm.value[`name`]}`);
+        console.log(`Current form status ${this.contactForm.status}`);
     }
 }
